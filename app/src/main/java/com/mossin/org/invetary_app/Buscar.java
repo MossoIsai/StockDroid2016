f0aa40b217d10c9f75a56d85d1d3b2c4c4bca31f
mossin <ing.isaimosso@gmail.com>
2015-03-19 18:45:25 -0600

    commit

--- app/src/main/java/com/mossin/org/invetary_app/Buscar.java
+++ app/src/main/java/com/mossin/org/invetary_app/Buscar.java
@@ -1,10 +1,14 @@
 package com.mossin.org.invetary_app;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 
 public class Buscar extends ActionBarActivity {
@@ -19,18 +23,20 @@ public class Buscar extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_buscar, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_buscar,menu);
+         /*
+        SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView)menu.findItem(R.id.busqueda).getActionView();*/
         return true;
+
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent;
         int id = item.getItemId();
-        if (id == R.id.buscar) {
-
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
